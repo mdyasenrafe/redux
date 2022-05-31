53aefea365d24e789758472f676593ced52dd7e8
Md yasen rafe <mdyasenrafe@gmail.com>
2022-05-31 21:40:43 +0600

    multiple reducer done and middleware working

--- basic/multiple_reducers.js
+++ basic/multiple_reducers.js
@@ -1,12 +1,18 @@
-const { createStore } = require("redux");
+const { createStore, combineReducers } = require("redux");
 
 // type
 const get_product = "GET_PRODUCT";
 const add_prdocut = "ADD_PRODUCT";
+const add_cart = "ADD_CART";
+const get_cart = "GET_CART";
 
 const intialProducts = {
   products: ["school management", "amar hotel"],
   count: 2,
+};
+const intitalCart = {
+  carts: ["school management"],
+  count: 1,
 };
 
 const getPrdocuts = () => {
@@ -18,6 +24,18 @@ const addProduct = (product) => {
   return {
     type: add_prdocut,
     payload: product,
+  };
+};
+
+const getCart = () => {
+  return {
+    type: get_cart,
+  };
+};
+const addCart = (payload) => {
+  return {
+    type: add_cart,
+    payload: payload,
   };
 };
 // reduxers
@@ -34,13 +52,37 @@ const productsReducer = (state = intialProducts, action) => {
         count: state.count + 1,
       };
     default:
-      state;
+      return state;
   }
 };
 
-const store = createStore(productsReducer);
+const CartReducer = (state = intitalCart, action) => {
+  switch (action.type) {
+    case get_cart:
+      return {
+        ...state,
+      };
+    case add_cart:
+      return {
+        ...state,
+        carts: [...state.carts, action.payload],
+        count: state.count + 1,
+      };
+    default:
+      return state;
+  }
+};
+const rootReducers = combineReducers({
+  cartR: CartReducer,
+  productR: productsReducer,
+});
+
+const store = createStore(rootReducers);
 
 store.subscribe(() => {
   console.log(store.getState());
 });
 store.dispatch(getPrdocuts());
+store.dispatch(addProduct("e-commerce"));
+store.dispatch(getCart());
+store.dispatch(addCart("e-commerce"));
