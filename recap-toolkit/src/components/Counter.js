fe80fab7cf6119ca39cb9e5a2923497e1e0572d0
mdyasenrafe <mdyasenrafe@gmail.com>
2023-07-31 00:28:42 +0600

    redux toolkit counter working

--- recap-toolkit/src/components/Counter.js
+++ recap-toolkit/src/components/Counter.js
@@ -1,19 +1,29 @@
 import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import styles from "./Counter.module.css";
+import { decrement, increment } from "../features/counter/counterSlice";
 
 export function Counter() {
   const dispatch = useDispatch();
   const [incrementAmount, setIncrementAmount] = useState("2");
+  const count = useSelector((state) => state.counter.count);
 
   return (
     <div>
       <div className={styles.row}>
-        <button className={styles.button} aria-label="Decrement value">
+        <button
+          onClick={() => dispatch(decrement(1))}
+          className={styles.button}
+          aria-label="Decrement value"
+        >
           -
         </button>
-        <span className={styles.value}>{incrementAmount}</span>
-        <button className={styles.button} aria-label="Increment value">
+        <span className={styles.value}>{count}</span>
+        <button
+          onClick={() => dispatch(increment(1))}
+          className={styles.button}
+          aria-label="Increment value"
+        >
           +
         </button>
       </div>
@@ -25,8 +35,18 @@ export function Counter() {
           onChange={(e) => setIncrementAmount(e.target.value)}
         />
         <button className={styles.button}>Add Amount</button>
-        <button className={styles.asyncButton}>Add Async</button>
-        <button className={styles.button}>Add If Odd</button>
+        <button
+          onClick={() => dispatch(increment(2))}
+          className={styles.asyncButton}
+        >
+          Add Async
+        </button>
+        <button
+          onClick={() => dispatch(increment(3))}
+          className={styles.button}
+        >
+          Add If Odd
+        </button>
       </div>
     </div>
   );
