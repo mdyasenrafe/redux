ce252e5dea561f0dc678c89cb43110aa8d1ede72
Md yasen rafe <mdyasenrafe@gmail.com>
2024-06-28 23:40:48 +0600

    redux setup added

--- advance-redux/src/App.tsx
+++ advance-redux/src/App.tsx
@@ -1,10 +1,9 @@
-import { Button } from "./components/ui/button";
+import Todo from "./pages/todo/Todo";
 
 function App() {
   return (
     <>
-      <h1>My Todo</h1>
-      <Button>My button</Button>
+      <Todo />
     </>
   );
 }
