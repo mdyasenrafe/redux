2bdf4923320177924a072c47ec57dde452306770
Md yasen rafe <mdyasenrafe@gmail.com>
2024-06-29 01:31:31 +0600

    droddown added

--- advance-redux/src/components/Todo/TodoContainer/TodoContainer.tsx
+++ advance-redux/src/components/Todo/TodoContainer/TodoContainer.tsx
@@ -1,5 +1,4 @@
-import { Button } from "@/components/ui/button";
-import { TodoCard } from "..";
+import { TodoCard, TodoFilter } from "..";
 import AddTodoModal from "../AddTodoModal/AddTodoModal";
 
 export function TodoContainer() {
@@ -7,7 +6,7 @@ export function TodoContainer() {
     <div>
       <div className="flex justify-between items-center mb-5">
         <AddTodoModal />
-        <Button>Filter</Button>
+        <TodoFilter />
       </div>
       <div className="bg-primary-gradient w-full h-full rounded-xl p-[5px]">
         <div className="bg-white p-5 w-full h-full rounded-lg space-y-3">
--- advance-redux/src/components/Todo/TodoFilter/TodoFilter.tsx
+++ advance-redux/src/components/Todo/TodoFilter/TodoFilter.tsx
@@ -1,3 +1,31 @@
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useState } from "react";
+
 export function TodoFilter() {
-  return <div>TodoFilter</div>;
+  const [position, setPosition] = useState("high");
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button>Filter</Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56">
+        <DropdownMenuLabel>Filter by priority</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuRadioGroup value={position} onValueChange={setPosition}>
+          <DropdownMenuRadioItem value="high">High</DropdownMenuRadioItem>
+          <DropdownMenuRadioItem value="medium">Medium</DropdownMenuRadioItem>
+          <DropdownMenuRadioItem value="low">Low</DropdownMenuRadioItem>
+        </DropdownMenuRadioGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 }
